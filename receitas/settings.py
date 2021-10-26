e59bfad299d4a8dacfee2d44d1c0e47c13cffd6b
Bernardo Ribeiro <bernardosr25@gmail.com>
2021-10-26 15:15:45 -0300

    feito a adição do postgre

--- receitas/settings.py
+++ receitas/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'receitas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'server_receita',
+        'USER': 'postgres',
+        'PASSWORD': '123456789',
+        'HOST': 'localhost'
     }
 }
 
