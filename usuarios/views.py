cd67e3ba3af2233dcb17c45ce6cf8d630732635c
Bernardo Ribeiro <bernardosr25@gmail.com>
2021-10-28 12:03:50 -0300

    criado a ferramenta para logar no sistema

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import  User
+from django.contrib  import  auth
 
 def cadastro(request):
     if request.method == 'POST':
@@ -35,8 +36,15 @@ def login(request):
         if email == '' or senha == '':
             print("os campos usuário ou senha estão vazios")
             return redirect('login')
+
         print(email, senha)
-        return redirect('dashboard')
+        if User.objects.filter(email=email).exists():
+            nome = User.objects.filter(email=email).values_list('username', flat= True).get()
+            user = auth.authenticate(request, username=nome, password=senha )
+            if user is not None:
+                auth.login(request, user)
+                print("login feito ")
+                return redirect('dashboard')
     return render(request, 'usuarios/login.html')
 
 def logout(request):
