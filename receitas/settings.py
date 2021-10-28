f42da23bdf844581388a22cbf7291c52b046fbd6
Bernardo Ribeiro <bernardosr25@gmail.com>
2021-10-28 10:54:39 -0300

    feito a inserção das paginas de cadastro e login

--- receitas/settings.py
+++ receitas/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'receitas.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'receitasapp/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
